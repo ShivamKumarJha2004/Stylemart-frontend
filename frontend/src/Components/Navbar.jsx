847bcadff6f6a1ddcb2d2e4f76ea9aa8b96561b4
Shivam Jha <108573467+ShivamKumarJha2004@users.noreply.github.com>
2024-10-23 15:05:41 +0530

    Update Navbar.jsx

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -107,7 +107,7 @@ function Navbar({ isDarkMode, toggleDarkMode }) {
               <button
                 type="button"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
-                onClick={<LoginSignup />}
+                
               >
                 Login
               </button>
